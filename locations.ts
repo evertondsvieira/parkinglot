01b98ab49e2d270dfd242348d898ca6016f19875
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-04-20 23:06:26 -0300

    :pencil: chore: mock data

---
+++ locations.ts
@@ -0,0 +1,7 @@
+export const parkingLotData = [
+  { id: 1, name: "Estacionamento Planalto", latitude: -25.4258832, "longitude": -49.2708856, price: 15, image: "https://autopark.com.br/wp-content/uploads/2019/12/imagem-destaque.png" },  
+  { id: 2, name: "Master Park Estacionamento", latitude: -25.4266734, "longitude": -49.2612588000000, price: 14, image: "https://autopark.com.br/wp-content/uploads/2019/12/imagem-destaque.png" },  
+  { id: 3, name: "Estacionamento Radar Park", latitude: -25.4210551, "longitude": -49.2705594, price: 12, image: "https://autopark.com.br/wp-content/uploads/2019/12/imagem-destaque.png" },  
+  { id: 4, name: "Park Concórdia", latitude: -25.4259023, "longitude": -49.2725604, price: 8, image: "https://autopark.com.br/wp-content/uploads/2019/12/imagem-destaque.png" }, 
+  { id: 5, name: "Maranata Estacionamento", latitude: -25.426986, "longitude": -49.270764, price: 15, image: "https://autopark.com.br/wp-content/uploads/2019/12/imagem-destaque.png" }, 
+]
