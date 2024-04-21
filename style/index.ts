10d26a9812b6d81237001c8f317f7e8ab1cd1b37
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-04-20 23:05:32 -0300

    :lipstick: chore: criado paleta de estilos

---
+++ style/index.ts
@@ -0,0 +1,25 @@
+const brand = {
+  main: "#EE6123",
+  secondary: "#FF9F1C",
+};
+
+const marker = {
+  main: "#107547",
+};
+
+const button = {
+  info: "#2D93AD",
+  success: "#26ac38",
+};
+
+const text = {
+  main: "#f3f3f3",
+  contrast: "#000000",
+};
+
+export const styled = {
+  text,
+  brand,
+  marker,
+  button,
+};
