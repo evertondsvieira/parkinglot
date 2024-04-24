d56dee8697cbb361f5eb0df07425b3e25f11bc08
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-04-23 23:53:36 -0300

    :truck: chore: movendo componente

---
+++ components/CustomTextInput.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, TextInput, KeyboardType } from "react-native";
+
+interface InputProps {
+  label: string;
+  value: string;
+  onChangeText: (text: string) => void;
+  placeholder?: string;
+  keyboardType?: KeyboardType;
+}
+
+export const CustomTextInput = (props: InputProps) => {
+  const { label, value, onChangeText, keyboardType, placeholder } = props;
+
+  return (
+    <View>
+      <Text style={{ fontWeight: "500" }}>{label}</Text>
+      <TextInput
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        keyboardType={keyboardType}
+        style={{ borderWidth: 1, borderColor: "#ccc", borderRadius: 8, padding: 8 }}
+      />
+    </View>
+  );
+};
