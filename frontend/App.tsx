180e1d1395a14bf98b824e9f83ace475da29cf33
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-05-19 14:14:01 -0300

    :sparkles: feat: chamando toast no app

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,19 @@
 import "react-native-gesture-handler";
+import { Fragment } from "react";
 import Routes from "./routes";
 import { LocationContextProvider } from "./context/Location";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Toast from "react-native-toast-message";
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <LocationContextProvider>
-        <Routes />
-      </LocationContextProvider>
-    </GestureHandlerRootView>
+    <Fragment>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <LocationContextProvider>
+          <Routes />
+        </LocationContextProvider>
+      </GestureHandlerRootView>
+      <Toast position="bottom" bottomOffset={20} />
+    </Fragment>
   );
 }
