fd36da2b5835f3abe679b02ff45cc5ab170419c1
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-06-05 21:40:42 -0300

    :sparkles: feat: consumindo firebase

--- frontend/pages/AccountPage/AccountPage.tsx
+++ frontend/pages/AccountPage/AccountPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Dimensions, Text, View } from "react-native";
 import { CustomTextInput } from "../../components/CustomTextInput";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -7,63 +7,54 @@ import Fontisto from "@expo/vector-icons/Fontisto";
 import { styled } from "../../style";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { RootStackList } from "../../@types";
+import { FIREBASE_AUTH, FIRESTORE_DB } from "../../firebaseConfig";
 
 export const AccountPage: React.FC = () => {
   const { button, brand } = styled;
   const { width } = Dimensions.get("window");
+  const auth = FIREBASE_AUTH;
+  const firestore = FIRESTORE_DB;
 
-  const [selectedVehicle, setSelectedVehicle] = useState<
-    "car" | "motorcycle" | null
-  >(null);
+  const [selectedVehicle, setSelectedVehicle] = useState<"car" | "motorcycle" | null>(null);
+
+  const [formData, setFormData] = useState({
+    fullName: "demo",
+    phone: "",
+    vehiclePlate: "",
+    cpf: "",
+  });
 
   const navigation = useNavigation<NavigationProp<RootStackList, "Mapa">>();
-
+  
   const toggleLocation = () => {
     navigation.navigate("Mapa");
   };
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "space-between",
-        margin: 12,
-      }}
-    >
-      <View style={{ width: "100%", gap: 8 }}>
-        <CustomTextInput
-          label="Nome Completo"
-          onChangeText={console.log}
-          placeholder="Digite seu nome completo"
-          value=""
-        />
-        <CustomTextInput
-          label="Telefone"
-          onChangeText={console.log}
-          placeholder="Digite seu telefone"
-          value=""
-        />
-        <CustomTextInput
-          label="Placa do Veículo"
-          onChangeText={console.log}
-          placeholder="Digite a placa do veículo"
-          value=""
-        />
-        <CustomTextInput
-          label="CPF"
-          onChangeText={console.log}
-          placeholder="Digite seu CPF"
-          value=""
-        />
+  const handleChange = (name: string, value: string) => {
+    setFormData({ ...formData, [name]: value });
+  };
 
-        <View
-          style={{
-            margin: 12,
-            flexDirection: "row",
-            justifyContent: "center",
-          }}
-        >
+  const fields = [
+    { label: "Nome Completo", placeholder: "Digite seu nome completo", value: formData.fullName, name: "fullName" },
+    { label: "Telefone", placeholder: "Digite seu telefone", value: formData.phone, name: "phone" },
+    { label: "Placa do Veículo", placeholder: "Digite a placa do veículo", value: formData.vehiclePlate, name: "vehiclePlate" },
+    { label: "CPF", placeholder: "Digite seu CPF", value: formData.cpf, name: "cpf" }
+  ];
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "space-between", margin: 12 }}>
+      <View style={{ width: "100%", gap: 8 }}>
+        {fields.map((field, index) => (
+          <CustomTextInput
+            key={index}
+            label={field.label}
+            onChangeText={(text) => handleChange(field.name, text)}
+            placeholder={field.placeholder}
+            value={field.value}
+          />
+        ))}
+
+        <View style={{ margin: 12, flexDirection: "row", justifyContent: "center" }}>
           <TouchableOpacity
             onPress={() => setSelectedVehicle("car")}
             style={{
@@ -72,20 +63,11 @@ export const AccountPage: React.FC = () => {
               padding: 16,
               borderRadius: 4,
               width: (width * 0.93) / 2,
-              backgroundColor:
-                selectedVehicle === "car" ? brand.main : "transparent",
+              backgroundColor: selectedVehicle === "car" ? brand.main : "transparent",
             }}
           >
-            <FontAwesome
-              name="car"
-              size={24}
-              color={selectedVehicle === "car" ? "#fff" : "#000"}
-            />
-            <Text
-              style={{ color: selectedVehicle === "car" ? "#fff" : "#000" }}
-            >
-              Automóvel
-            </Text>
+            <FontAwesome name="car" size={24} color={selectedVehicle === "car" ? "#fff" : "#000"} />
+            <Text style={{ color: selectedVehicle === "car" ? "#fff" : "#000" }}>Automóvel</Text>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => setSelectedVehicle("motorcycle")}
@@ -95,24 +77,11 @@ export const AccountPage: React.FC = () => {
               padding: 16,
               borderRadius: 4,
               width: (width * 0.93) / 2,
-              backgroundColor:
-                selectedVehicle === "motorcycle"
-                  ? brand.secondary
-                  : "transparent",
+              backgroundColor: selectedVehicle === "motorcycle" ? brand.secondary : "transparent",
             }}
           >
-            <Fontisto
-              name="motorcycle"
-              size={24}
-              color={selectedVehicle === "motorcycle" ? "#fff" : "#000"}
-            />
-            <Text
-              style={{
-                color: selectedVehicle === "motorcycle" ? "#fff" : "#000",
-              }}
-            >
-              Motocicleta
-            </Text>
+            <Fontisto name="motorcycle" size={24} color={selectedVehicle === "motorcycle" ? "#fff" : "#000"} />
+            <Text style={{ color: selectedVehicle === "motorcycle" ? "#fff" : "#000" }}>Motocicleta</Text>
           </TouchableOpacity>
         </View>
       </View>
--- frontend/pages/LoginPage/LoginPage.tsx
+++ frontend/pages/LoginPage/LoginPage.tsx
@@ -10,29 +10,71 @@ import {
 import { styled } from "../../style";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { RootStackList } from "../../@types";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { FIREBASE_AUTH } from "../../firebaseConfig";
+import { toastMessage } from "../../utils/toastMessage";
 
 export const LoginPage = () => {
   const { brand } = styled;
 
-  const navigation = useNavigation<NavigationProp<RootStackList, 'Mapa'>>();
+  const navigation =
+    useNavigation<NavigationProp<RootStackList, "Mapa" | "Registro">>();
+
+  const auth = FIREBASE_AUTH;
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Login bem-sucedido:", user);
+        navigation.navigate("Mapa");
+      })
+      .catch((error) => {
+        console.error("Erro ao fazer login:", error);
+        toastMessage({ title: "Erro ao fazer login", type: "error" })
+      });
+  };
+
+  const goToRegister = () => {
+    navigation.navigate("Registro");
+  };
 
   return (
     <View style={[styles.container, { backgroundColor: brand.main }]}>
       <View style={{ alignItems: "center", justifyContent: "center" }}>
         <Image style={styles.logo} source={require("../../assets/logo1.png")} />
-        <TextInput style={styles.input} placeholder="Nome de usuário" />
         <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          value={password}
+          onChangeText={setPassword}
           style={styles.input}
           placeholder="Senha"
           secureTextEntry={true}
         />
-        <TouchableOpacity 
-          onPress={() => navigation.navigate('Mapa')} 
-          style={styles.button}
-        >
+        <TouchableOpacity onPress={handleSignIn} style={styles.button}>
           <Text style={styles.buttonText}>Logar</Text>
         </TouchableOpacity>
       </View>
+      <TouchableOpacity
+        onPress={goToRegister}
+        style={{
+          justifyContent: "flex-end",
+          alignItems: "flex-end",
+          paddingRight: 60,
+          paddingVertical: 10,
+        }}
+      >
+        <Text style={{ color: "#fff", textDecorationLine: "underline" }}>
+          Registre-se
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -52,11 +94,11 @@ const styles = StyleSheet.create({
     width: 300,
     height: 40,
     borderWidth: 1,
-    borderColor: "#ccc",
+    borderColor: "#fff",
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
   },
   button: {
     width: 300,
@@ -68,6 +110,5 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: "#fff",
-    fontWeight: "bold",
   },
 });
